43dcd4ab5f9d43d6f47a1dd983825b07552ec630
Rahul <esds@Rahuls-Mac-Mini.local>
2015-07-09 17:58:15 +0530

    Initial Commit

---
+++ SpinnerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpinnerTest
+//
+//  Created by Rahul on 7/9/15.
+//  Copyright (c) 2015 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
